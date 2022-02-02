f5efad0df4fc08cd0e6254492be5ea9f4f1513e4
nayab <nayabchamp22@gmail.com>
2022-02-02 05:33:04 -0800

    nft landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import "./App.css";
-import Banner from "./Components/Banner/Banner";
+import Banner from "./Sections/Banner/Banner";
+import Faqs from "./Sections/Faqs/Faqs";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
-import RoadMap from "./Components/RoadMap/RoadMap";
-import WalletButton from "./WalletButton";
-
+import RoadMap from "./Sections/RoadMap/RoadMap";
+import ConnectWalletButton from "./ConnectWalletButton";
 function App() {
   return (
     <>
       <Header />
       <Banner />
       <RoadMap />
+      <Faqs text="dhhdhd" title="dgdgdd" />
       <Footer />
-      {/* <WalletButton /> */}
+      {/* <ConnectWalletButton /> */}
     </>
   );
 }
